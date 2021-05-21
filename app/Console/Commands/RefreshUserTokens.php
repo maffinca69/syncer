f16c8dc7436ffed11d5185c99b90bdb5916f4bd9
Maffinca69 <lollidark@mail.ru>
2021-05-21 11:25:36 +0400

    Refresh token command

---
+++ app/Console/Commands/RefreshUserTokens.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace App\Console\Commands;
+
+
+use App\Jobs\CheckNewFavoriteTracksJob;
+use App\Models\User;
+use App\Services\AuthService;
+use Illuminate\Console\Command;
+use function Symfony\Component\String\s;
+
+class RefreshUserTokens  extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'token:refresh';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update refresh token for all user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param AuthService $service
+     * @return mixed
+     */
+    public function handle(AuthService $service)
+    {
+        $this->info('Начинаю проверку');
+        $users = User::all();
+        foreach ($users as $user) {
+            $token = $user->refresh_token;
+            $service->refreshToken($token);
+        }
+
+        $this->info('Токены обновлены');
+        return true;
+    }
+}
